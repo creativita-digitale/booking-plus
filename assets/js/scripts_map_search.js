374149320414a2182320c487e5670c014498683a
alexrighetto <alexrighetto@gmail.com>
2017-07-29 13:29:19 +0200

    map update

--- assets/js/scripts_map_search.js
+++ assets/js/scripts_map_search.js
@@ -4,29 +4,32 @@
 
 		// Creating a new map
 		var map = new google.maps.Map(document.getElementById("search_map"), {
-          center: new google.maps.LatLng(57.9, 14.6),
-          zoom: 6,
+          center: new google.maps.LatLng(45.438384, 10.991622),
+          zoom: 13,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         });
 
 
 		// Creating the JSON data
 		var json = script_data.markers;
-window.alert(json);
+//window.alert(json);
 		// Creating a global infoWindow object that will be reused by all markers
 		var infoWindow = new google.maps.InfoWindow();
-
+		 var bounds = new google.maps.LatLngBounds();
+		
 		// Looping through the JSON data
 		for (var i = 0, length = json.length; i < length; i++) {
 			var data = json[i],
 				latLng = new google.maps.LatLng(data.lat, data.lng);
-
+bounds.extend( latLng);
 			// Creating a marker and putting it on the map
 			var marker = new google.maps.Marker({
 				position: latLng,
 				map: map,
 				title: data.title
 			});
+			 
+                   
 
 			// Creating a closure to retain the correct data, notice how I pass the current data in the loop into the closure (marker, data)
 			(function(marker, data) {
@@ -41,6 +44,7 @@ window.alert(json);
 			})(marker, data);
 
 		}
+		 map.fitBounds(bounds);
 
 	}
 
